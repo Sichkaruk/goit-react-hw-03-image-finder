26326335c8f5a34964a33b3ce5e7b0e0ff1068e1
Bohdan Sichkaruk <82411127+Sichkaruk@users.noreply.github.com>
2022-06-14 15:48:32 +0300

    setup and structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import "./App.styled.js";
+import React, { Component } from "react";
+import {
+  Container,
+  Title,
+  Logo,
+  Button,
+  SubTitle,
+  List,
+  ListItem,
+  Link,
+} from "./App.styled";
+import logo from "./img/logo3.png";
+import toastMsg from "./utils/toast";
 
-function App() {
-  return <div className="App">Flowers for Algernon</div>;
+class App extends Component {
+  onClick = () => {
+    console.log("click");
+    toastMsg("Flowers for Algernon", "info");
+  };
+
+  render() {
+    return (
+      <Container>
+        <Title>
+          <Logo src={logo} alt="logo" width="50px" />
+          Image Finder
+        </Title>
+        <SubTitle>Template list</SubTitle>
+        <List>
+          <ListItem>Example list item</ListItem>
+          <ListItem>Example list item</ListItem>
+          <ListItem>Example list item</ListItem>
+        </List>
+        <Link href="#">Example link</Link>
+        <br /> <br />
+        <Button type="button" onClick={this.onClick}>
+          Click me
+        </Button>
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { IconContext } from "react-icons";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./constants/theme";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <IconContext.Provider
+        value={{
+          size: "1.3em",
+          style: { verticalAlign: "middle", marginRight: "8px" },
+        }}
+      >
+        <ToastContainer />
+        <App />
+      </IconContext.Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
